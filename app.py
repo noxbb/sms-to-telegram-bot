ee6c6d6e0cf9fa5475c51eb8579c421cd80eded3
noxbb <noxbbot@gmail.com>
2025-03-02 22:06:16 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from send_sms_to_telegram import send_sms_to_telegram
+
+app = Flask(__name__)
+
+# API endpoint для получения SMS от APK
+@app.route('/sms', methods=['POST'])
+def receive_sms():
+    data = request.get_json()  # Получаем данные JSON из POST-запроса
+    if not data or 'sender' not in data or 'message' not in data:
+        return jsonify({"error": "Invalid data"}), 400
+
+    # Отправляем полученные данные в Telegram
+    response = send_sms_to_telegram(data)
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
